807770d5a1f872023a17084a527e12602f204e25
Mamushe <tewodroslemma64@gmail.com>
2022-06-20 09:30:41 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './app/modals/item/item.component';
+import { ConfirmationComponent } from './modals/confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule
