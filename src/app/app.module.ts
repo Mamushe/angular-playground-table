b5ca4dbace89352f83d1489ab8a7a0a8f8aa5fe5
Mamushe <tewodroslemma64@gmail.com>
2022-06-20 11:13:20 -0400

    display table done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ItemComponent } from './modals/item/item.component'; 
+import { ItemComponent } from './modals/item/item.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { MatTableModule } from '@angular/material/table';
+import {
+  MatInputModule,
+} from '@angular/material/input';
+// import {  } from '@angular/material/searchbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemComponent
+    ItemComponent,
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    MatTableModule,
+    MatInputModule,
+    BrowserAnimationsModule,
   ], 
   providers: [],
   bootstrap: [AppComponent]
